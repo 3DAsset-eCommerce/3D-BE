ec4a4724beaa20887c55520a9ba22fd7c47e441c
HJoo <yoon3798@naver.com>
2023-06-28 10:50:38 +0900

    chore : 메인에 push

--- src/main/java/com/phoenix/assetbe/model/auth/VerifiedCodeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.phoenix.assetbe.model.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VerifiedCodeRepository extends JpaRepository<VerifiedCode, Long> {
-}
